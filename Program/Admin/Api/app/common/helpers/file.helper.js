af71a74b2b7bad9c2e75688930b85695f7eb2821
Nguyễn Chí Trung <100399135+21424092-trungnc@users.noreply.github.com>
2022-12-04 17:31:55 +0700

    Update file.helper.js

--- Program/Admin/Api/app/common/helpers/file.helper.js
+++ Program/Admin/Api/app/common/helpers/file.helper.js
@@ -66,7 +66,7 @@ const saveTmpBase64File = (pathFile, base64) => {
 };
 
 const getPathStorage = () => {
-  return `D:\\TrungNC6\\Quick-Solution-Team-main\\Program\\Admin\\Portal\\public`;
+  return 'D:\\TrungNC6\\Quick-Solution-Team-main\\Program\\Admin\\Portal\\public';
 };
 
 const getPathUpload = () => {
